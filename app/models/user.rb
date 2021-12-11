cff49812bfb960f04b8db8b4faecf37c3bba2fcb
Aaron Barnes <aaron.barnes1@hotmail.com>
2021-12-10 20:13:02 -0600

    add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_many :posts
 end
